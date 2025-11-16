ded9e6f1ba90d28ca151d8c7a1bce1377e536215
anvar <hestorikal@mail.ru>
2025-11-17 01:08:36 +0400

    а

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -15,10 +15,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.macOS:
         return macos;
       case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return windows;
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
@@ -32,37 +29,54 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBn_4_example_key',
-    appId: '1:439851419572:web:example',
+    apiKey: 'AIzaSyAx5J_U2NoGAtdxF9WwDnIbOBfiNs2wcjU',
+    appId: '1:439851419572:web:ef9f9ef2dbfa0f346a94e1',
     messagingSenderId: '439851419572',
     projectId: 'katenka-74591',
     authDomain: 'katenka-74591.firebaseapp.com',
     databaseURL: 'https://katenka-74591-default-rtdb.firebaseio.com',
+    storageBucket: 'katenka-74591.firebasestorage.app',
+    measurementId: 'G-8LHG1CYX80',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBn_4_example_key',
-    appId: '1:439851419572:android:example',
+    apiKey: 'AIzaSyB-FJ--DQVflP9oqbfOsBITq9426w3YwlA',
+    appId: '1:439851419572:android:f1a77c0f05793a296a94e1',
     messagingSenderId: '439851419572',
     projectId: 'katenka-74591',
     databaseURL: 'https://katenka-74591-default-rtdb.firebaseio.com',
+    storageBucket: 'katenka-74591.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBn_4_example_key',
-    appId: '1:439851419572:ios:example',
+    apiKey: 'AIzaSyAY_bSzgHx646SfbO2X03vquCE6IhOfpr4',
+    appId: '1:439851419572:ios:95ddcf513f1b35bb6a94e1',
     messagingSenderId: '439851419572',
     projectId: 'katenka-74591',
     databaseURL: 'https://katenka-74591-default-rtdb.firebaseio.com',
+    storageBucket: 'katenka-74591.firebasestorage.app',
     iosBundleId: 'com.example.time',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyBn_4_example_key',
-    appId: '1:439851419572:macos:example',
+    apiKey: 'AIzaSyAY_bSzgHx646SfbO2X03vquCE6IhOfpr4',
+    appId: '1:439851419572:ios:95ddcf513f1b35bb6a94e1',
     messagingSenderId: '439851419572',
     projectId: 'katenka-74591',
     databaseURL: 'https://katenka-74591-default-rtdb.firebaseio.com',
+    storageBucket: 'katenka-74591.firebasestorage.app',
     iosBundleId: 'com.example.time',
   );
+
+  static const FirebaseOptions windows = FirebaseOptions(
+    apiKey: 'AIzaSyAx5J_U2NoGAtdxF9WwDnIbOBfiNs2wcjU',
+    appId: '1:439851419572:web:b6d8813b888b745d6a94e1',
+    messagingSenderId: '439851419572',
+    projectId: 'katenka-74591',
+    authDomain: 'katenka-74591.firebaseapp.com',
+    databaseURL: 'https://katenka-74591-default-rtdb.firebaseio.com',
+    storageBucket: 'katenka-74591.firebasestorage.app',
+    measurementId: 'G-TEXCL3KY32',
+  );
+
 }
